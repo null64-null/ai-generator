d63f9a11afeb35e809d3db2406496fa70b8af54d
null64-null <hay.tech64@gmail.com>
2024-02-24 00:22:09 +0900

    make image_generator.py

--- image_generation.py
+++ image_generation.py
@@ -2,7 +2,7 @@ import sys, os
 import numpy as np
 
 from propagetion.loss import CrossEntropyError
-from learning import Supervised_learning
+from learning import Gun
 from layer import generate_layers
 
 sys.path.append(os.pardir)
@@ -12,23 +12,23 @@ from mnist.mnist import load_mnist
 ##### input #####
 # data
 # 最終的に、データセットをまとめたpklファイルを読み込む形にする（以下）
-# 入力値は、trainとtestのpklファイルパスと、画像サイズとする
+# 入力値は、authのpklファイルパスと、画像サイズとする
 '''
 # file path, image shape
-train_file_path = 'train.pkl'           #dummy
-train_image_shape = [5000, 3, 32, 32]   #dummy
-test_file_path = 'test.pkl'             #dummy
-test_image_shape = [1000, 3, 32, 32]    #dummy
+auth_file_path = 'auth.pkl'           #dummy
+auth_image_shape = [5000, 3, 32, 32]  #dummy
 '''
-
-# batch size
-batch_size = 100
 
 # network
 # oh = 1 + (h + 2 * pad - fh) / st
 error = CrossEntropyError()
 lerning_rate = 0.1
-layer_params = [
+
+generator_layers_params = [
+    
+]
+
+discriminator_layers_params = [
     # (n, c, h, w) = (batch_size, 1, 28, 28)
     {
         'layer_type': 'convolution_layer',
@@ -49,6 +49,13 @@ layer_params = [
     },
     # (h, w) = (batch_size, 1*1*10) = (batch_size, 10)
     {
+        'layer_type': 'affine_layer',
+        'params': {
+            'layer_size': [10, 1],
+        }
+    },
+    # (h, w) = (batch_size, 1)
+    {
         'layer_type': 'soft_max',
         'params': {}
     },
@@ -56,56 +63,49 @@ layer_params = [
 
 # learning, checking setting
 iters = 500
-accuracy_check_span = 100
-check_mask_size_train = 50
-check_mask_size_test = 50
+picture_check_span = 100
 
 
 ##### process input #####
 
 # dummy mnist
-(x_train, t_train), (x_test, t_test) = load_mnist(one_hot_label=True, normalize=True)
-train_data_length = x_train.shape[0]
-test_data_length = x_test.shape[0]
-x_train = x_train.reshape(train_data_length, 1, 28, 28)
-x_test = x_test.reshape(test_data_length, 1, 28, 28)
+(x_auth, t_auth), (_, _) = load_mnist(one_hot_label=True, normalize=True)
+auth_data_length = x_auth.shape[0]
+x_auth = x_auth.reshape(auth_data_length, 1, 28, 28)
+
 '''
-x_train, t_train, _ = generate_data_from_pkl(train_file_path, train_image_shape)
-x_test, t_test, _ = generate_data_from_pkl(test_file_path, test_image_shape)
+x_auth, t_auth, _ = generate_data_from_pkl(auth_file_path, auth_image_shape)
 '''
 
 data = {
-    'x_train': x_train,
-    't_train': t_train,
-    'x_test': x_test,
-    't_test': t_test,
+    'x_auth': x_auth,
+    't_auth': t_auth,
 }
 
 learning_params = {
     'lerning_rate': lerning_rate,
-    'batch_size': batch_size,
     'iters': iters,
 }
 
 checking_params = {
-    'accuracy_check_span': accuracy_check_span,
-    'check_mask_size': 50,
+    'picture_check_span': picture_check_span,
 }
 
-layers = generate_layers(layer_params)
+generator_layers = generate_layers(generator_layers_params)
+discriminator_layers = generate_layers(discriminator_layers_params)
 
 
 ##### learn #####
-learning = Supervised_learning (
+learning = Gun (
     data = data,
-    layers = layers,
+    generator_layers = generator_layers,
+    discriminator_layers = discriminator_layers,
     error = error,
     learning_params = learning_params,
     checking_params = checking_params,
-    isShowProgress = True,
-    isShowGraph = True,
-    isShowResult = True,
-    isShowFilters = True,
+    is_show_progress = True,
+    is_show_pictures = True,
+    is_show_result = True,
 )
 
 learning.learn()
